1384d02d91bde3b10c7f3ddc35b5c02f3b2956e2
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-10 17:48:36 +0000

    created journey_history instance variable and modified #touch_out and added journey history attr_reader

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,10 +5,11 @@ class Oystercard
   MIN_CHARGE = 1
 
 
-  attr_reader :balance, :station
+  attr_reader :balance, :entry_station, :journey_history
 
   def initialize
     @balance = 0
+    @journey_history = []
   end
 
   def top_up(money)
@@ -18,13 +19,14 @@ class Oystercard
 
   def touch_in(tube_stop)
     fail "Insufficient funds. Balance: #{balance}" if balance < MIN_CHARGE
-    @station = tube_stop
+    @entry_station = tube_stop
     @in_use = true
   end
 
-  def touch_out
+  def touch_out(tube_stop)
     deduct(MIN_CHARGE)
-    @station = nil
+    @journey_history << {entry_station: @entry_station, exit_station: tube_stop}
+    @entry_station = nil
     @in_use = false
   end
 
