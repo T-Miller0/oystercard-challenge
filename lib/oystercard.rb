49360befd4db875cf89c76254a977875e47a6fe5
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-10 16:21:45 +0000

    added touch in/out support

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,9 @@
 class Oystercard
 
+  MAX_BALANCE = 90
+
+  MIN_BALANCE = 1
+
   attr_reader :balance
 
   def initialize
@@ -7,6 +11,24 @@ class Oystercard
   end
 
   def top_up(money)
+    fail "Balance would exceed #{MAX_BALANCE}" if balance + money > MAX_BALANCE
     @balance += money
+  end
+
+  def deduct(money)
+    @balance -= money
+  end
+
+  def touch_in
+    fail "Insufficient funds. Balance: #{balance}" if balance < MIN_BALANCE
+    @in_use = true
+  end
+
+  def touch_out
+    @in_use = false
+  end
+
+  def in_journey?
+    @in_use
   end
 end
